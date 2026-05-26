f144218d2978798854b205e322052e8a309c8dbf
marco76tv <marco.sottana@gmail.com>
2026-05-26 09:15:59 +0200

    .

--- app/Console/Commands/DatabaseSchemaExporterCommand.php
+++ app/Console/Commands/DatabaseSchemaExporterCommand.php
@@ -8,6 +8,7 @@ use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Symfony\Component\Console\Output\OutputInterface;
 
 use function Safe\json_encode;
 
@@ -67,7 +68,10 @@ class DatabaseSchemaExporterCommand extends Command
             'connection' => $connection,
             'tables' => [],
         ];
-
+        $output = $this->getOutput();
+        if (! $output instanceof OutputInterface) {
+            throw new Exception('Output is not an instance of OutputInterface');
+        }
         $progressBar = $output->createProgressBar(count($tables));
         $progressBar->start();
 
